f2e29ae3fca5946e70c32cc609fd9953611833c0
whoelse <whoelse@git.com>
2025-07-12 00:39:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+
+  return (
+	<h1 className='text-9xl'>Yo</h1>
+  )
+}
+
